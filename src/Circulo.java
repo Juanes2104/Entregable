7382645944328a33cd408a19b7cb70d462683425
Juanes <cdjuanes210403@gmail.com>
2024-05-03 16:24:48 -0500

    Encapsulamiento y Herencia - Implementación de Subclases por Juanes

---
+++ src/Circulo.java
@@ -0,0 +1,8 @@
+public class Circulo extends Figuras_geometricas {
+    private float radio;
+
+    public Circulo(String nombre, String color, float radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+}
---
+++ src/Rectangulo.java
@@ -0,0 +1,8 @@
+public class Rectangulo extends Figuras_geometricas{
+    private float Longitud;
+
+    public Rectangulo(String nombre, String color, float longitud) {
+        super(nombre, color);
+        Longitud = longitud;
+    }
+}
