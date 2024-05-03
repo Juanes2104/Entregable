ed4da5496c725fd62f65a7935c26e1451f3e7935
Juanes <cdjuanes210403@gmail.com>
2024-05-03 15:49:48 -0500

    Encapsulamiento - Implementación de Validaciones por Juanes

--- src/Persona.java
+++ src/Persona.java
@@ -3,4 +3,47 @@ public class Persona {
     private String fecha_nacimiento;
     private Integer edad;
     private Integer año_actual;
+
+    public Persona(String fecha_nacimiento, String nombre, Integer edad, Integer año_actual) {
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.año_actual = año_actual;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public Integer getEdad() {
+        if (edad < 0){
+            return 0;
+        } else {
+            return edad;
+        }
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getAño_actual() {
+        return año_actual;
+    }
+
+    public void setAño_actual(Integer año_actual) {
+        this.año_actual = año_actual;
+    }
 }
