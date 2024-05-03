52d07a12abfabc79cecf3f7d43b137beba4d7b93
Juanes <cdjuanes210403@gmail.com>
2024-05-03 16:22:22 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Juanes

--- src/Figuras_geometricas.java
+++ src/Figuras_geometricas.java
@@ -6,4 +6,20 @@ public class Figuras_geometricas {
         this.nombre = nombre;
         this.color = color;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
