5f4009ea372a777220d0981c0cc236040121e156
Juanes <cdjuanes210403@gmail.com>
2024-05-03 16:20:58 -0500

    Encapsulamiento y Herencia - Implementación de Clase FiguraGeometrica por Juanes

---
+++ src/Figuras_geometricas.java
@@ -0,0 +1,9 @@
+public class Figuras_geometricas {
+    private String nombre;
+    private String color;
+
+    public Figuras_geometricas(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+}
