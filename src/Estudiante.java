55798eca46686e03c28ea1b2234247b750931ab8
Juanes <cdjuanes210403@gmail.com>
2024-05-03 16:04:36 -0500

    Herencia - Implementación de Propiedad adicional para el grado por Juanes

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,8 @@
-public class Estudiante {
+public class Estudiante extends Persona {
+    private Integer grado;
+
+    public Estudiante(String fecha_nacimiento, String nombre, Integer edad, Integer año_actual, Integer grado) {
+        super(fecha_nacimiento, nombre, edad, año_actual);
+        this.grado = grado;
+    }
 }
